974ad02e45a76b7667065b9eb2d26c044c81306c
arun <arun.saboori@gmail.com>
2022-07-24 23:02:15 +0430

    updating  features

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,13 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('/css/welcome.css') }}">
     <title>welcome</title>
 </head>
 <body>
-    <h1>page welcome</h1>
-    {{-- @foreach ($todo as $newtodos )
-        {{dd($newtodos)}}
-    @endforeach --}}
-    
+
+
+   <a href="{{ route('todo.item') }}" class="h1">welcome page</a>
+
 </body>
 </html>
